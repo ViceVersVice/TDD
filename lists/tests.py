633a7923955015ffc6623136198b6f6cc1a33749
ViceVersa <kotohawk@gmail.com>
2019-06-12 14:24:59 +0200

    Refactored code to use assertTemplateUsed instead of manual check

--- lists/tests.py
+++ lists/tests.py
@@ -1,20 +1,13 @@
 from django.test import TestCase
 from django.urls import resolve
 from django.http import HttpRequest
+from django.template.loader import render_to_string
 
 from .views import SomeView
 
 # Create your tests here.
 class SomeTest(TestCase):
 
-    def test_some_url(self):
-        founded_view = resolve("/some-view/")
-        self.assertEqual(SomeView, founded_view.func)
-
     def test_SomeView_return_correct_html(self):
-        request = HttpRequest()
-        response = SomeView(request)
-        html = response.content.decode("utf8")
-        self.assertTrue(html.startswith("<html>"))
-        self.assertIn("<title>To-Do</title>", html)
-        self.assertTrue(html.endswith("</html>"))
+        response = self.client.get("/some-view/")
+        self.assertTemplateUsed(response, "home.html")

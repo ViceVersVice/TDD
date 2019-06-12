1fefbd4809a740afbaad2b174de70df851a345c5
ViceVersa <kotohawk@gmail.com>
2019-06-12 20:39:02 +0200

    refactored some repetetive actions

--- lists/tests.py
+++ lists/tests.py
@@ -11,3 +11,9 @@ class SomeTest(TestCase):
     def test_SomeView_return_correct_html(self):
         response = self.client.get("/some-view/")
         self.assertTemplateUsed(response, "home.html")
+
+    def test_can_save_a_POST_request(self):
+        response = self.client.post("/some-view/",
+                                    data={"item_text": "a new list item"})
+        self.assertIn("a new list item", response.content.decode("utf8"))
+        self.assertTemplateUsed(response, "home.html")
--- lists/views.py
+++ lists/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def SomeView(request):
-    return render(request, "home.html")
+    #if request.method == "POST":
+    return render(request, "home.html",
+                  {"new_item_text": request.POST.get("item_text", "")})
+    #return render(request, "home.html")

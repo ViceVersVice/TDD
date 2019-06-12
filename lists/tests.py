5b47669b2cb2676e669ac296b33504ab3a25c269
ViceVersa <kotohawk@gmail.com>
2019-06-12 20:57:08 +0200

    Added Item model test

--- lists/tests.py
+++ lists/tests.py
@@ -4,6 +4,7 @@ from django.http import HttpRequest
 from django.template.loader import render_to_string
 
 from .views import SomeView
+from lists.models import Item
 
 # Create your tests here.
 class SomeTest(TestCase):
@@ -17,3 +18,16 @@ class SomeTest(TestCase):
                                     data={"item_text": "a new list item"})
         self.assertIn("a new list item", response.content.decode("utf8"))
         self.assertTemplateUsed(response, "home.html")
+
+class ItemModelTest(TestCase):
+
+    def test_CR_item(self):
+        first_item = Item.objects.create(text="Some 1 item")
+        second_item = Item.objects.create(text="Some 2 item")
+
+        all_items = Item.objects.all()
+        self.assertEqual(2, all_items.count())
+        first_saved_item = all_items[0]
+        second_saved_item = all_items[1]
+        self.assertEqual(first_saved_item.text, "Some 1 item")
+        self.assertEqual(second_saved_item.text, "Some 2 item")
